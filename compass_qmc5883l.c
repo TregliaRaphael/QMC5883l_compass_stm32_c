7138be30f4406c101d0488ee998641526dd73eb5
raphael.treglia <32885615+TregliaRaphael@users.noreply.github.com>
2020-11-13 11:11:00 +0100

    Add files via upload

---
+++ compass_qmc5883l.c
@@ -0,0 +1,94 @@
+#include "compass_qmc5883l.h"
+
+bool qmc5883lInit(I2C_HandleTypeDef *hi2c)
+{
+
+    bool ack = true;
+    uint8_t trash = 0x01;
+    uint8_t ini = QMC5883L_MODE_CONTINUOUS | QMC5883L_ODR_200HZ | QMC5883L_OSR_512 | QMC5883L_RNG_8G;
+    ack = ack && !HAL_I2C_Mem_Write(hi2c, QMC5883L_MAG_I2C_ADDRESS, 0x0B, I2C_MEMADD_SIZE_8BIT, &trash, 1, TM_GLOB);
+    ack = ack && !HAL_I2C_Mem_Write(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_CONF1, I2C_MEMADD_SIZE_8BIT, &ini, 1, TM_GLOB);
+
+    if (!ack) {
+        return false;
+    }
+
+    return true;
+}
+
+bool qmc5883lRead(I2C_HandleTypeDef *hi2c, int16_t magData[3])
+{
+    uint8_t status;
+    uint8_t buf[6];
+
+    // set magData to zero for case of failed read
+    magData[0] = 0;
+    magData[1] = 0;
+    magData[2] = 0;
+
+    bool ack = true;
+    ack = ack && !HAL_I2C_Mem_Read(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_STATUS, I2C_MEMADD_SIZE_8BIT, &status, 1, TM_GLOB);
+
+    if (!ack || (status & 0x04) == 0) {
+        return false;
+    }
+
+    ack = ack && !HAL_I2C_Mem_Read(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_DATA_OUTPUT_X, I2C_MEMADD_SIZE_8BIT, buf, 6, TM_GLOB);
+    if (!ack) {
+        return false;
+    }
+
+    magData[0] = (int16_t)(buf[1] << 8 | buf[0]);
+    magData[1] = (int16_t)(buf[3] << 8 | buf[2]);
+    magData[2] = (int16_t)(buf[5] << 8 | buf[4]);
+
+    return true;
+}
+
+bool qmc5883lDetect(I2C_HandleTypeDef *hi2c)
+{
+
+    // Must write reset first  - don't care about the result
+    //busWriteRegister(busdev, QMC5883L_REG_CONF2, QMC5883L_RST);
+    uint8_t reset = QMC5883L_RST;
+    HAL_I2C_Mem_Write(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_CONF2, I2C_MEMADD_SIZE_8BIT, &reset, 1, TM_GLOB);
+    
+    //delay lol
+    int tt = 0;
+    while (tt < 1000)
+	    tt++;
+
+    uint8_t sig = 0;
+
+    //HAL_StatusTypeDef det = HAL_I2C_Mem_Read(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_ID, I2C_MEMADD_SIZE_8BIT, &sig, 1, TM_GLOB);
+    bool ack = true;
+    ack = ack && !HAL_I2C_Mem_Read(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_ID, I2C_MEMADD_SIZE_8BIT, &sig, 1, TM_GLOB);
+    if (ack && sig == QMC5883_ID_VAL) {
+        // Should be in standby mode after soft reset and sensor is really present
+        // Reading ChipID of 0xFF alone is not sufficient to be sure the QMC is present
+        //ack = busReadRegisterBuffer(busdev, QMC5883L_REG_CONF1, &sig, 1);
+        ack = ack && !HAL_I2C_Mem_Read(hi2c, QMC5883L_MAG_I2C_ADDRESS, QMC5883L_REG_CONF1, I2C_MEMADD_SIZE_8BIT, &sig, 1, TM_GLOB);
+        if (ack && sig != QMC5883L_MODE_STANDBY) {
+            return false;
+        }
+        //magDev->init = qmc5883lInit;
+        //magDev->read = qmc5883lRead;
+	//lets do some mess
+        return true;
+    }
+
+    return false;
+}
+
+uint16_t searchDevice(I2C_HandleTypeDef *hi2c)
+{
+  HAL_StatusTypeDef result;
+  uint8_t i;
+  for (i=1; i<128; i++)
+  {
+          result = HAL_I2C_IsDeviceReady(hi2c, (uint16_t)(i<<1), 2, 2);
+          if (result == HAL_OK)
+		return i;
+  }
+  return 0;
+}
